6a530ee0a36a573d5baa9cf92cec4c21d1d858c5
sowerbee <sowerby.dave@gmail.com>
2019-01-03 10:20:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function create(env, ctx) {
     });
 
     var appPages = {
-	"/hud.html":"hud.html",
+	"/hud.html":"hud.html",    
         "/clock-color.html":"clock-color.html",
         "/admin":"adminindex.html",
         "/profile":"profileindex.html",
